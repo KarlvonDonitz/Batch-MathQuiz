16146a3a1b3a70607410cb800e4d90309d512403
KarlvonDonitz <32868638+KarlvonDonitz@users.noreply.github.com>
2019-04-10 15:36:38 +0800

    update Project3
    input txt file for check

--- Project3.bat
+++ Project3.bat
@@ -1,4 +1,18 @@
 @echo off
-set /a ans=%1
-if %ans% EQU %2 ( echo correct) else ( echo worng)
+setlocal ENABLEDELAYEDEXPANSION
+title Math_Quiz_Checker
+cls
+set path=%~dp0
+set path=%path%Checker.txt
+c:>%path%
+:file_reader
+for /f "tokens=1,2 delims==" %%i in (%1) DO (
+set question=%%i
+set answer=%%j
+set /a check=!question!
+if !answer! EQU !check! echo !question!=!answer! is Correct>>%path%
+if !answer! NEQ !check! echo !question!=!answer! is Wrong>>%path%
+)
+:end
+echo All problems in %1 have been checked and exported to %path%
 pause
